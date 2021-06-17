4ac81a7821fc80d02802b029ade456c25a055b64
Albertus Agung <albertus.ag@outlook.com>
2021-06-18 00:24:17 +0900

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestAPI
+{
+    public partial class Form1 : Form
+    {
+        private GoogleTest google;
+
+        public Form1()
+        {
+            InitializeComponent();
+            google = new GoogleTest();
+            google.GenerateAudio("Welcome to Japan, the land of rising sun!!", "output-wavenet.mp3");
+        }
+    }
+}
---
+++ GoogleTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.TextToSpeech.V1;
+using System.IO;
+
+namespace TestAPI
+{
+    class GoogleTest
+    {
+        private string jsonName = "key.json";
+
+        private string baseDir = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+        private TextToSpeechClient client;
+
+        public GoogleTest() {
+            Console.WriteLine("Init Google Test");
+            AuthExplicit();
+        }
+
+        private void AuthExplicit() {
+            string jsonPath = $"{baseDir}/{jsonName}";
+
+            Console.WriteLine("Init Auth");
+            TextToSpeechClientBuilder builder = new TextToSpeechClientBuilder
+            {
+                CredentialsPath = jsonPath
+            };
+
+            client = builder.Build();
+        }
+
+        public void ListVoices(string code = "En") {
+            
+            var responseList = client.ListVoices(code);
+            foreach (var voice in responseList.Voices)
+            {
+                Console.WriteLine($"{voice.Name} ({voice.SsmlGender}); Language codes: {string.Join(", ", voice.LanguageCodes)}");
+            }
+        }
+
+        public void GenerateAudio(string textInput, string outputFile = "output.mp3") {
+            var input = new SynthesisInput
+            {
+                Text = textInput
+            };
+
+            // Build the voice request.
+            var voiceSelection = new VoiceSelectionParams
+            {
+                LanguageCode = "en-US",
+                Name = "en-US-Wavenet-C",
+                SsmlGender = SsmlVoiceGender.Female
+            };
+
+            // Specify the type of audio file.
+            var audioConfig = new AudioConfig
+            {
+                AudioEncoding = AudioEncoding.Mp3
+            };
+
+            // Perform the text-to-speech request.
+            var response = client.SynthesizeSpeech(input, voiceSelection, audioConfig);
+
+            // Write the response to the output file.
+            //string outputFile = "output-voting.mp3";
+            string outputPath = $"{baseDir}/{outputFile}";
+            using (var output = File.Create(outputPath))
+            {
+                response.AudioContent.WriteTo(output);
+            }
+            Console.WriteLine($"Audio content written to file \"{outputFile}\"");
+        }
+    }
+}
